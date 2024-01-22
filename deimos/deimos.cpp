45777de75470289afea8fd09cc496faba0680b49
lucia <llcsaturnaliam@gmail.com>
2024-01-21 22:28:46 -0600

    silly stuff

--- deimos/deimos.cpp
+++ deimos/deimos.cpp
@@ -1,6 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+void __stdcall attachedMain(HMODULE hModule) {
+
+}
+
 int __stdcall DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
   if (dwReason == DLL_PROCESS_ATTACH) {
     MessageBoxA(0, "hi", "hi", MB_OK);
