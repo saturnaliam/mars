22af224481851063fff8a52b33ab6699305bb352
lucia <llcsaturnaliam@gmail.com>
2024-01-21 21:54:13 -0600

    no more cmake

--- deimos/deimos.cpp
+++ deimos/deimos.cpp
@@ -1,3 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+int __stdcall DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
+  if (dwReason == DLL_PROCESS_ATTACH) {
+    MessageBoxA(0, "hi", "hi", MB_OK);
+  }
+
+  return 1;
+}
